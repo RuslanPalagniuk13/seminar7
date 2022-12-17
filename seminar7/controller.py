543fea0734c34ddcc5b70edb6462a8656deb6c19
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-12-17 11:42:31 +0300

    внес изменения так как выскакивала ошибка

--- seminar7/controller.py
+++ seminar7/controller.py
@@ -19,20 +19,21 @@ def input_data():
     brith_name = input("Введите дату рождения: ")
     phone_number = input("Введите номер контакта: ")
     note = input("Введите категорию контакта: ")
-    today = dt.now().strftime('%m%d/%Y/ - %H:%M')
+    today = dt.now().strftime('%m.%d.%Y - %H:%M')
     return [last_name, first_name, middle_name, brith_name, phone_number, note, today]
 
+
 def choice_sep():
-    sep = ", " #input("Введите разделитель: ")
-#    if sep == "":
-#        sep = None
-    return sep
+    sep = input(", ")
+    #if sep == "":
+    #    sep = None
+    # return sep
 
 def choice_todo():
     print("Доступные операции с телефонной книгой:\n\
-    1 - импорт\n\
-    2 - экспорт\n\
-    3 - поиск контакта\n")
+    1 - импорт;\n\
+    2 - экспорт;\n\
+    3 - поиск контакта.")
     ch = input("Введите цифру: ")
     if ch == '1':
         sep = choice_sep()
@@ -42,10 +43,11 @@ def choice_todo():
         print_data(data)
     else:
         word = input("Введите данные для поиска: ")
-        word = word.upper()
         data = export_data()
         item = search_data(word, data)
         if item != None:
-            print_data(data)
+            print("Фамилия".center(15), "Имя".center(15), "Отчество".center(15), "Дата рождения".center(15), "Телефон".center(15), "Категория".center(20), "Дата и Время внесения".center(20))
+            print("-"*130)
+            print(item[0].center(15), item[1].center(15), item[2].center(15), item[3].center(15), item[4].center(15), item[5].center(20), item[6].center(20))
         else:
             print("Данные не обнаружены")
